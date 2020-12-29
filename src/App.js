06130deb8a0ccdda015718128cb6586400c362ec
gunwoongPark <olympiodoros@naver.com>
2020-12-29 16:18:27 +0900

    JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import Hello from './Hello'
+import "./App.css"
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  }
   return (
-    <div>
+    <>
       <Hello />
-    </div>
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
   );
 }
 
