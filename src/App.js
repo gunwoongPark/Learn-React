c9f607149b5bbea67ebde5f0f27cfd601f239296
gunwoongPark <olympiodoros@naver.com>
2020-12-29 20:12:58 +0900

    UseRef to create a variable within a component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import React, { useRef } from 'react';
 import UserList from "./UserList";
 
 function App() {
+  const users = [
+    {
+      id: 1,
+      username: "박건웅",
+      email: "bung1438@gmail.com"
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: "tester@example.com"
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: "liz@example.com"
+    }
+  ];
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    console.log(nextId.current);
+    nextId.current += 1;
+  }
+
   return (
-    <UserList />
+    <UserList users={users} />
   );
 }
 
--- src/UserList.js
+++ src/UserList.js
@@ -8,24 +8,7 @@ function User({ user }) {
     )
 }
 
-function UserList() {
-    const users = [
-        {
-            id: 1,
-            username: "박건웅",
-            email: "bung1438@gmail.com"
-        },
-        {
-            id: 2,
-            username: 'tester',
-            email: "tester@example.com"
-        },
-        {
-            id: 3,
-            username: 'liz',
-            email: "liz@example.com"
-        }
-    ];
+function UserList({ users }) {
 
     return (
         <div>
