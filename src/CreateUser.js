98080003825f58f5671e33f032441c0991f0ab57
20160450 <olympiodoros@naver.com>
2021-01-03 17:30:16 +0900

    homwork again

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useContext, useRef, useCallback } from 'react';
+import { UserDispatch } from './App'
+import useInputs from "./useInputs";
 
-function CreateUser({ username, email, onChange, onCreate }) {
+function CreateUser() {
+    const [{ username, email }, onChange, reset] = useInputs({
+        username: '',
+        email: ''
+    })
+
+    const nextId = useRef(4)
+
+    const dispatch = useContext(UserDispatch);
+
+    const onCreate = useCallback(() => {
+        dispatch({
+            type: 'CREATE_USER',
+            user: {
+                id: nextId.current,
+                username,
+                email
+            }
+        });
+        reset();
+        nextId.current += 1;
+    }, [username, email, reset, dispatch])
+
     return (
         <div>
             <input
