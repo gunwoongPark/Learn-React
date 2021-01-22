8ab7d97382efa13b32d55c958126117613232cc7
gunwoongPark <olympiodoros@naver.com>
2021-01-22 17:00:16 +0900

    I told you to make the important data react state, not variables.

--- coding-apple/src/App.js
+++ coding-apple/src/App.js
@@ -1,15 +1,33 @@
 import "./App.css";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 
 function App() {
-  let posts = { color: "blue", fontSize: "30px" };
+  // 자주 바뀌는 중요한 데이터는 state 사용
+  // 웹앱처럼 새로고침이 최소화 하기위해
+  let [글제목1, 글제목변경1] = useState("남자 코트 추천1");
+  let [글제목2, 글제목변경2] = useState("남자 코트 추천2");
+  let [글제목3, 글제목변경3] = useState("남자 코트 추천3");
+
+  let posts = "강남 고기 맛집";
 
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={posts}>개발 Blog</div>
+        <div>개발 Blog</div>
       </div>
-      <h4>{posts}</h4>
+      <div className="list">
+        <h3>{글제목1}</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+
+        <h3>{글제목2}</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+
+        <h3>{글제목3}</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
     </div>
   );
 }
