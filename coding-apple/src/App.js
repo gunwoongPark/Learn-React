8d9fac7139452ab7c8b1ec72cf4432ff422a54fc
gunwoongPark <olympiodoros@naver.com>
2021-01-23 16:20:29 +0900

    React Component: When you feel the urge to reduce many divs to one word

--- coding-apple/src/App.js
+++ coding-apple/src/App.js
@@ -13,18 +13,17 @@ function App() {
   let [따봉, 따봉변경] = useState(0);
   let posts = "강남 고기 맛집";
 
-  function 제목바꾸기() {
-    var newArray = [...글제목];
-    newArray[0] = "여자 코트 추천";
-    글제목변경(newArray);
-  }
+  // function 제목바꾸기() {
+  //   var newArray = [...글제목];
+  //   newArray[0] = "여자 코트 추천";
+  //   글제목변경(newArray);
+  // }
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={제목바꾸기}>버튼</button>
       <div className="list">
         <h3>
           {글제목[0]}
@@ -41,14 +40,28 @@ function App() {
         <hr />
 
         <h3>{글제목[1]}</h3>
-        <p>2월 17일 발행</p>
+        <p>2월 18일 발행</p>
         <hr />
 
         <h3>{글제목[2]}</h3>
-        <p>2월 17일 발행</p>
+        <p>2월 19일 발행</p>
         <hr />
       </div>
+
+      <Modal></Modal>
     </div>
+  );
+}
+
+function Modal() {
+  return (
+    <>
+      <div className="modal">
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+    </>
   );
 }
 
