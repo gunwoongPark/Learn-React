30b7496b7443f0934ffc09af1183d052660fef35
20160450 <olympiodoros@naver.com>
2021-02-16 01:15:17 +0900

    Classless CSS Styling with Styled-Components

--- coding-apple/shop/src/Detail.js
+++ coding-apple/shop/src/Detail.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { useHistory, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+let 박스 = styled.div`
+  padding: 20px;
+`;
+
+let 제목 = styled.h4`
+  font-size: 25px;
+  color: ${(props) => props.색상};
+`;
 
 function Detail(props) {
   let { id } = useParams();
-
-  let 찾은상품 = props.shoes.find(function (상품) {
-    return 상품.id == id;
-  });
-
   let history = useHistory();
+  let 찾은상품 = props.shoes.find((상품) => 상품.id == id);
 
   return (
     <div className="container">
+      <박스>
+        <제목>Detail</제목>
+      </박스>
       <div className="row">
         <div className="col-md-6">
           <img
