a70ab05d576ff2d466ec3cd41baab1d932db6ceb
20160450 <olympiodoros@naver.com>
2021-02-16 03:13:53 +0900

    How do I transfer the state if I make 3 components overlapped?

--- coding-apple/shop/src/App.js
+++ coding-apple/shop/src/App.js
@@ -8,6 +8,7 @@ import axios from "axios";
 
 function App() {
   let [shoes, shoes변경] = useState(Data);
+  let [재고, 재고변경] = useState([10, 11, 12]);
 
   return (
     <div className="App">
@@ -75,7 +76,7 @@ function App() {
         </Route>
 
         <Route path="/detail/:id">
-          <Detail shoes={shoes} />
+          <Detail shoes={shoes} 재고={재고} 재고변경={재고변경} />
         </Route>
 
         <Route path="/:id">
