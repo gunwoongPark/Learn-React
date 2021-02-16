de59c404d8edb33cad96416ebfa2daeb2fb2a1c5
gunwoongPark <olympiodoros@naver.com>
2021-02-16 19:20:03 +0900

    Tab Creation and React-Transition-group

--- coding-apple/shop/src/Detail.js
+++ coding-apple/shop/src/Detail.js
@@ -3,6 +3,9 @@ import { useHistory, useParams } from "react-router-dom";
 import styled from "styled-components";
 import "./Detail.scss";
 import { 재고context } from "./App.js";
+import { Nav } from "react-bootstrap";
+
+import { CSSTransition } from "react-transition-group";
 
 let 박스 = styled.div`
   padding: 20px;
@@ -16,6 +19,10 @@ let 제목 = styled.h4`
 function Detail(props) {
   let [alert, alert변경] = useState(true);
   let [inputData, inputData변경] = useState("");
+
+  let [누른탭, 누른탭변경] = useState(0);
+  let [스위치, 스위치변경] = useState(false);
+
   let 재고 = useContext(재고context);
 
   useEffect(() => {
@@ -76,8 +83,51 @@ function Detail(props) {
           </button>
         </div>
       </div>
+
+      <Nav className="mt-5" variant="tabs" defaultActiveKey="link-0">
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link-0"
+            onClick={() => {
+              스위치변경(false);
+              누른탭변경(0);
+            }}
+          >
+            Active
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link-1"
+            onClick={() => {
+              스위치변경(false);
+              누른탭변경(1);
+            }}
+          >
+            Option 2
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+
+      <CSSTransition in={스위치} classNames="wow" timeout={500}>
+        <TabContent 누른탭={누른탭} 스위치변경={스위치변경} />
+      </CSSTransition>
     </div>
   );
+}
+
+function TabContent(props) {
+  useEffect(() => {
+    props.스위치변경(true);
+  });
+
+  if (props.누른탭 === 0) {
+    return <div>0번째 내용입니다</div>;
+  } else if (props.누른탭 === 1) {
+    return <div>1번째 내용입니다</div>;
+  } else if (props.누른탭 === 2) {
+    return <div>2번째 내용입니다</div>;
+  }
 }
 
 function Info(props) {
