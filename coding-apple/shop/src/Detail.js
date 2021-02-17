a35ff73bf3207abbb8e643e8a8cdeca1706d75b7
gunwoongPark <olympiodoros@naver.com>
2021-02-17 17:49:00 +0900

    World's Easiest Redux 4: Data Can Be Shipped When Dispatched

--- coding-apple/shop/src/Detail.js
+++ coding-apple/shop/src/Detail.js
@@ -6,6 +6,7 @@ import { 재고context } from "./App.js";
 import { Nav } from "react-bootstrap";
 
 import { CSSTransition } from "react-transition-group";
+import { connect } from "react-redux";
 
 let 박스 = styled.div`
   padding: 20px;
@@ -69,6 +70,11 @@ function Detail(props) {
             className="btn btn-danger"
             onClick={() => {
               props.재고변경([9, 11, 12]);
+              props.dispatch({
+                type: "항목추가",
+                payload: { id: 2, name: "새로운상품", quan: 1 },
+              });
+              history.push("/cart");
             }}
           >
             주문하기
@@ -134,4 +140,11 @@ function Info(props) {
   return <p>재고 : {props.재고[0]}</p>;
 }
 
-export default Detail;
+function state를props화(state) {
+  return {
+    state: state.reducer,
+    alert열렸니: state.reducer2,
+  };
+}
+
+export default connect(state를props화)(Detail);
